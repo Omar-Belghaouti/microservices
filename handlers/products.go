8ad4fd6475763a47ff05fcc38cc11eca78d9cfd6
Omar-Belghaouti <omarbelghaouti@gmail.com>
2022-05-06 02:53:14 +0100

    adding products api

---
+++ handlers/products.go
@@ -0,0 +1,35 @@
+package handlers
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/Omar-Belghaouti/microservices/data"
+)
+
+type Products struct {
+	l *log.Logger
+}
+
+func NewProducts(l *log.Logger) *Products {
+	return &Products{l}
+}
+
+func (p *Products) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	p.l.Println("products handler")
+	if r.Method == http.MethodGet {
+		p.getProducts(w, r)
+		return
+	}
+	// catch all
+	w.WriteHeader(http.StatusMethodNotAllowed)
+}
+
+func (p *Products) getProducts(w http.ResponseWriter, r *http.Request) {
+	lp := data.GetProducts()
+	err := lp.ToJSON(w)
+	if err != nil {
+		http.Error(w, "Unable to marshal json", http.StatusInternalServerError)
+		return
+	}
+}
